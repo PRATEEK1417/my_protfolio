3ab6928cf27dfad1e4450c0dfbd06c43264878dc
PRATEEK1417 <36917852+PRATEEK1417@users.noreply.github.com>
2021-05-13 09:23:25 +0530

    commit

--- hello_world/views.py
+++ hello_world/views.py
@@ -3,3 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 def hello_world(request):
     return render(request,'index.html',{})
+
+
+def photography(request):
+    return render(request, "photography.html", {})
+
+def resume(request):
+    return render(request,"resume.html",{})
